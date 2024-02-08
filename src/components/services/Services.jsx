73ad601d0bccc706ccf33f843dfe7c7ff9aabf57
Arpi Arakelyan <arpinearakelyan01@gmail.com>
2024-02-08 08:16:44 +0400

    added changes of the services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import "./Services.css";
 import {
-  BsDatabaseFill,
   BsWindowDesktop,
-  BsBug,
   BsPalette,
+  BsCalculator,
+  BsFillBadge3DFill,
+  BsPeople
 } from "react-icons/bs";
-import { CiLock } from "react-icons/ci";
 import SeviceCard from "./SeviceCard";
-// import [photos] from '../../assets/services'
+import { DiGoogleAnalytics } from "react-icons/di";
+
 
 import background from "../../assets/services/12.jpg";
 
@@ -17,70 +18,74 @@ export const Services = () => {
     {
       id: 1,
       icon: BsWindowDesktop,
-      header: "Create Your Website",
+      title: "Վեբ կայքերի պատրաստում․",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quos ipsam,",
+        "Համաձայնեք, որ անհնար է պատկերացնել շահավետ բիզնես առանց իր «վիրտուալ այցեքարտի»՝ վեբ կայքի։ ",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti quae libero. Culpa iure optio, odio soluta voluptatem quam recusandae beatae totam vitae earum, sed temporibus nesciunt, magni aut eum?",
-      // photo: URL(background)
+        ["Universal Programming Company-ն (UPC) հնարավորություն կընձեռի Ձեզ ունենալ ցանկացած տեսակի և բարդության վեբ կայքեր։ UPC-ի աշխատանքային թիմում ներգրավված պրոֆեսիոնալ մասնագետները պատրաստակամ են Ձեզ համար ստեղծել, սպասարկել և արդիականացնել ցանկացած բարդության այցեքարտային, կորպորատիվ, կատալոգային, առցանց խանութների (online shopping), անձնական, լրատվական և այլ տեսակի responsive (բջջային ադապտիվ տարբերակ) վեբ կայքեր։ Վերջինիս առկայության դեպքում անհրաժեշտությունից ելնելով կարող ենք կատարել կայքի փոփխումներ, ավելացնել ցանկացած ֆունկցիոնալ ծառայություններ, առկայության դեպքում շտկել նաև առաջացած խնդիրները և այս ամենը մատուցվում է հնարավոր սեղմ ժամկետներում՝ մատչելի գներով։"],
+     
     },
     {
       id: 2,
-      icon: BsDatabaseFill,
-      header: "Database Management",
-      description:
-        "UPC provides top-notch database management solutions for small and medium businesses worldwide.",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti quae libero. Culpa iure optio, odio soluta voluptatem quam recusandae beatae totam vitae earum, sed temporibus nesciunt, magni aut eum?",
+      icon: DiGoogleAnalytics,
+      title: "Մարկետինգ",
+      description: 
+        "Ձեր բիզնեսի առաջխաղացման համար կարևոր դեր ունի մարկեթինգային ռազմավարության ստեղումն ու իրականացումը, ինչը և առաջարկում է UPC թիմը",
+      content: 
+        ['SMM առաջխաղացում', 'SEO առաջխաղացում','Digital Marketing (Google Ads /PPC, OLV/)','Local SEO (Google my business)','Marketing strategy/tactics','Marketing analysis','Social media marketing (full)','Content marketing','Content Creation'],
     },
     {
       id: 3,
       icon: BsPalette,
-      header: "UX & UI Design",
+      title: "UX / UI Դիզայն",
       description:
-        "Our team of UX designers creates easy-to-understand interfaces for all kinds of applications.",
+       "Մեր UX դիզայներների թիմը ստեղծում է հետաքրքիր և տարբերվող ինտերֆեյս՝ բոլոր տեսակի հավելվածների համար:",
       content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti quae libero. Culpa iure optio, odio soluta voluptatem quam recusandae beatae totam vitae earum, sed temporibus nesciunt, magni aut eum?",
+       ['Վեբ դիզայն', 'Գրաֆիկ դիզայն','Լոգոների դիզայն','Լոգոների 3D դիզայն'],
     },
     {
       id: 4,
-      icon: BsWindowDesktop,
-      header: "IT Consulting",
+      icon: BsPeople,
+      title: "Սպասարկում",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quos ipsam,",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti quae libero. Culpa iure optio, odio soluta voluptatem quam recusandae beatae totam vitae earum, sed temporibus nesciunt, magni aut eum?",
+        "UPC թիմը Ապահովում է Ձեր կայքի աշխատունակությունը և զարգացումը։",
+      content: 
+        ['Կայքերի սպասարկում', 'Հոսթինգ և դոմեն', 'Տեխնիկական աջակցություն'],
     },
     {
       id: 5,
-      icon: BsBug,
-      header: "QA & Testing",
+      icon: BsFillBadge3DFill,
+      title: "Եռաչափ (3D) մոդելավորում.",
       description:
-        "We pay a lot of attention to QA and Testing procedures to ensure the best quality of our software.",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti quae libero. Culpa iure optio, odio soluta voluptatem quam recusandae beatae totam vitae earum, sed temporibus nesciunt, magni aut eum?",
+        "Universal Programming Company-ը (UPC) մատուցում է բարձրորակ եռաչափ՝ 3D մոդելավորման ծառայություն։",
+      content: 
+        ['UPC-ի աշխատանքային թիմի համապատասխան մասնագետների համակարգչային գրաֆիկական հմտությունները  հնարավորություն կնձեռեն Ձեզ ունենալ ցանակացած իրական կամ երևակայական անշարժ գույքի, ստատիկ օբյեկտի կամ որևէ երևույթի եռաչափ՝ 3D ձևաչափով մոդել։  Մեր մոդելավորողները մասնագիտացված Autodesk 3ds Max ունիվերսալ ծրագրի կիրառմամբ՝ մանրակրկիտ ուշադրություն դարձնելով յուրաքանչյուր դետալներին կմոդելավորվեն ցանկացած բարդության և ծավալի, իրատեսական էֆֆեկտներով ճշգրիտ 3D մոդելներ։ Իրականացվում են նաև լոգոների, այցեքարտերի, բուկլետների, կատալոգների, մենյուների յուրօրինակ դիզայնով 3D մոդելավորումներ, որոնք բացառիկ հնարավորություն են ընձեռում, որպեսզի Դուք, Ձեր ընկերությունը և/կամ բիզնեսը գործունեության ոլորտում լինի ավելի ներկայանալի և աչքի ընկնող։']
+        
     },
     {
       id: 6,
-      icon: CiLock,
-      header: "Security",
+      icon: BsCalculator,
+      title: "Հաշվապահություն",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti",
+        "Ձեր բիզնեսի ամբողջ հաշվապահությունը կարող եք վստահել մեզ",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti quae libero. Culpa iure optio, odio soluta voluptatem quam recusandae beatae totam vitae earum, sed temporibus nesciunt, magni aut eum?",
+        ["նորաստեղծ կազմակերպությունների հաշվապահության ձևավորում","աշխատավարձի, հարկերի և սոցապ հատկացումների հաշվարկ","հաշվապահական աջակցություն","բանկային գործարքների վարում"],
     },
   ];
 
   const [CardInfo, setCardInfo] = useState(null);
 
-  function handelclick(data, d) {
+  function handelclick(e, data, d) {
     if (CardInfo === null) {
       const newCard = data.find((item) => item.id === d.id);
       if (newCard) {
         setCardInfo([newCard]);
+        console.log(e.target.parentElement)
+
       }
     }
   }
+ 
 
   return (
     <div
@@ -94,26 +99,23 @@ export const Services = () => {
           </div>
           <div className="contents">
             {servicesData.map((data) => (
-              <div className="services-first" key={data.id}>
+              <div className="services-first" key={data.id} 
+              >
                 <div>
                   <h3>
                     <data.icon color="#00C9B2FF" size={42} />
                   </h3>
-                  
-                    <h2>{data.header}</h2>
+                    <h2>{data.title}</h2>
                     <p>{data.description}</p> 
-                  
                 </div>
-          
-                  
-                  <button className="service_details">
-                    <span onClick={() => handelclick(servicesData, data)}>
-                      View details
+                  <button className="service_details" onClick={(e) => handelclick(e, servicesData, data)}>
+                    <span >
+                      Տեսնել Ավելին
                     </span>
                   </button>
               </div>
             ))}
-          {/* {CardInfo && <SeviceCard data={CardInfo} setCardInfo={setCardInfo} />} */}
+          {CardInfo && <SeviceCard data={CardInfo} setCardInfo={setCardInfo} />}
 
           </div>
       </div>
--- src/components/services/SeviceCard.jsx
+++ src/components/services/SeviceCard.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 
 export default function ServiceCard({ data, setCardInfo }) {
     const el = data[0];
-
     const handleClose = () => {
         setCardInfo(null);
     };
 
     return (
         <div className="cardClass">
-            <el.icon color='#00C9B2FF' size={48}/>
-            <h1>{el.header}</h1>
-            <p>{el.content}</p>
+            <el.icon color='#03616dae' size={48}/>
+            <ul><h4>{el.title}</h4>
+                {console.log(el)}
+                {
+                    el.content.map(elem =>{
+                        return <li className='card_li'>{elem}</li>
+                    })
+                }
+            </ul>
             <span onClick={handleClose}>X</span>
         </div>
     );
